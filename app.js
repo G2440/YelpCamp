f01c01a77355b1a23619a60c44d2e873ad297968
Gaurav Negi <42696032+G2440@users.noreply.github.com>
2019-08-02 23:43:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+var CampG = [
+    { name : "Random" , image : "https://picsum.photos/200"},
+    { name : "NRandom", image : "https://picsum.photos/200"},
+    { name : "NNRandom", image : "https://picsum.photos/200"},
+    { name : "NNRandom", image : "https://picsum.photos/200"},
+
+    { name : "NNRandom", image : "https://picsum.photos/200"},
+
+    { name : "NNRandom", image : "https://picsum.photos/200"},
+
+    { name : "NNRandom", image : "https://picsum.photos/200"},
+
+    { name : "NNRandom", image : "https://picsum.photos/200"},
+
+    { name : "NNRandom", image : "https://picsum.photos/200"},
+
+    { name : "NNRandom", image : "https://picsum.photos/200"},
+    { name : "NNRandom", image : "https://picsum.photos/200"},
+    { name : "NNRandom", image : "https://picsum.photos/200"},
+    { name : "NNRandom", image : "https://picsum.photos/200"},
+    { name : "NNRandom", image : "https://picsum.photos/200"}
+
+    
+];
+app.get("/", function(req,res){
+
+    res.render("landing");
+});
+
+app.get("/campgrounds" , function(req,res){
+
+
+    res.render("campgrounds", {data : CampG} );
+});
+app.post("/campgrounds", function(req,res){
+    var ng  = req.body.name ; 
+    var ni = req.body.image;
+    var newCampG =  { name : ng , image : ni };
+    CampG.push(newCampG);
+    res.redirect("/campgrounds");
+      
+});
+app.get("/campgrounds/new",function(req,res){
+      res.render("new");
+
+});
+
+
+app.listen(8080,function(){
+
+    console.log("Here We GOOOO!!!!");
+});
